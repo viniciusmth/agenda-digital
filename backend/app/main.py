47200017cc825322751a0422d4c72316e02a81f9
viniciusmth <viniciusmatheus40@hotmail.com>
2025-09-09 10:52:47 -0300

    teste frontend 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = ["https://agenda-digital-iota.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers= ["*"]
